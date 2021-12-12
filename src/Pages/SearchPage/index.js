5924c6c1537a0cb3317fd3eca68c35ddea708cda
zahraaghazadeh <zahraaliaghazadeh@gmail.com>
2021-12-12 00:54:42 -0800

    added style to search page

--- src/Pages/SearchPage/index.js
+++ src/Pages/SearchPage/index.js
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
-import { useLocation } from "react-router-dom";
-import { queryJobs } from "../../service/api";
-import { parse } from 'query-string';
+import {useLocation} from "react-router-dom";
+import {queryJobs} from "../../service/api";
+import {parse} from 'query-string';
 import JobCard from "../../Components/JobCard";
-import { URL_JOB_DETAILS_DYNAMIC } from "../../constants/routes";
-
+import {URL_JOB_DETAILS_DYNAMIC} from "../../constants/routes";
 
 export default function SearchPage() {
   const location = useLocation();
@@ -27,23 +26,34 @@ export default function SearchPage() {
   }, []);
 
   return (
-    <div>
-      {
-        jobs && jobs.length === 0 && (
-          <div>
-            <h2>No jobs found for: {query}</h2>
+      <div>
+        <div className={"container"}>
+          <div className={"row"}>
+            <div className={"col col-md-auto-12"}>
+
+
+              {
+                  jobs && jobs.length === 0 && (
+                      <div>
+                        <h2>No jobs found for: {query}</h2>
+                      </div>
+                  )
+              }
+              {
+                  jobs && jobs.map((job, index) => (
+                      <a key={index} href={URL_JOB_DETAILS_DYNAMIC(job._id)}
+                         style={{
+                           textDecoration: 'none'
+                         }}>
+                        <JobCard key={index} job={job}/>
+                      </a>
+                  ))
+              }
+            </div>
+
           </div>
-        )
-      }
-      {
-        jobs && jobs.map((job, index) => (
-          <a key={index} href={URL_JOB_DETAILS_DYNAMIC(job._id)} style={{
-            textDecoration: 'none'
-          }}>
-            <JobCard key={index} job={job} />
-          </a>
-        ))
-      }
-    </div>
+
+        </div>
+      </div>
   )
 }
