72552a0c3aaa64359edfe8f1bd98f86bc142fedf
zahraaghazadeh <zahraaliaghazadeh@gmail.com>
2021-12-12 00:43:18 -0800

    added gradiant to job card

--- src/Components/JobCard/index.js
+++ src/Components/JobCard/index.js
@@ -2,13 +2,17 @@ import React from "react";
 
 const JobCard = (props) => {
   const { job } = props;
+  const color1 = "#ffd6a5";
+  const color2 = "#e0e1dd";
+
   return (
     <div style={{
       color: "#2b2d42",
       margin: '10px 20px',
-      border: 'solid',
+      border: 'solid 2px',
       borderRadius: '10px',
-      padding: '10px'
+      padding: '10px',
+      background: `linear-gradient(to bottom,  ${color1} 0%,${color2} 100%)`
     }}>
       <h1>{job.title}</h1>
       <p>{job.companyName}</p>
@@ -19,6 +23,7 @@ const JobCard = (props) => {
         src={job.image}
         style={{
           height: '50px',
+          borderRadius: '50%'
         }}
       />
     </div>
