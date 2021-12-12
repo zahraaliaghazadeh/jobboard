d28ddb6dd5bedea4d90c4b575a41e1d894c8175b
zahraaghazadeh <zahraaliaghazadeh@gmail.com>
2021-12-12 07:19:54 -0800

    removed navbar animation due to bug, replaces by other styling and icons

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { useState } from "react";
+import {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
-import { URL_FAVORITES, URL_LOGIN, URL_MY_JOBS, URL_ROOT, URL_SIGNUP } from "../../constants/routes";
-import { logout } from "../../service/api";
+import {
+  URL_FAVORITES,
+  URL_LOGIN,
+  URL_MY_JOBS,
+  URL_ROOT,
+  URL_SIGNUP
+} from "../../constants/routes";
+import {logout} from "../../service/api";
+import {
+  Heart,
+  Person,
+  PersonCheck,
+  DoorClosed,
+  FileEarmarkLock,
+  Key,
+  HouseDoor
+} from 'react-bootstrap-icons';
 
 export default function Navbar(props) {
-  const { username } = props;
+  const {username} = props;
 
   const [showHamburgerContent, setShowHamburgerContent] = useState(false);
 
@@ -35,98 +50,97 @@ export default function Navbar(props) {
   }
 
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        {/* <a className="navbar-brand logo" href="#">Battleship</a> */}
-        <a className="logo " href={URL_ROOT}>
-          <div className="logo"><img src="/favicon.ico" alt="logo of a battleship"></img></div>
-          <div className="navbar-brand logo">Job Board</div>
-        </a>
-        <button
-          onClick={() => toggleHamburger()}
-          className="navbar-toggler hamburger"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="logo " href={URL_ROOT}>
+            <div className="logo"><img src="/favicon.ico"
+                                       alt="logo of a battleship"></img></div>
+            <div className="navbar-brand logo">Job Board</div>
+          </a>
+          <button
+              onClick={() => toggleHamburger()}
+              className="navbar-toggler hamburger"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <div className={"collapse navbar-collapse w-100 order-1 order-md-0 dual-collapse2 " + show} id="navbarSupportedContent">
-          {/*<ul className="navbar-nav mr-auto">*/}
-          {/* */}
-          {/*</ul>*/}
+          <div
+              className={"collapse navbar-collapse w-100 order-1 order-md-0 dual-collapse2 "
+                  + show} id="navbarSupportedContent">
 
-
-
-          <ul className="nav navbar-nav">
-            {/*Home*/}
-            <li className="nav-item active">
-              <a className="nav-link sr-only" href="/">
-                Home
-              </a>
-            </li>
-            {
-              !username && (
-                <li className="nav-item active" onClick={onLoginClick}>
-                  {/*<button type="button" className="btn btn-warning login-button" onClick={onLoginClick}>*/}
-                    <a className="nav-link">
-                      Login
-                    </a>
-                  {/*</button>*/}
-                </li>
-              )
-            }
-            {
-              !username && (
-                <li className="nav-item active">
-                  {/*<button type="button" className="btn btn-warning signup-button">*/}
-                    <a className="nav-link" href={URL_SIGNUP}>
-                      Sign Up
-                    </a>
-                  {/*</button>*/}
-                </li>
-              )
-            }
-            {
-              username &&
+            <ul className="nav navbar-nav">
+              {/*Home*/}
               <li className="nav-item active">
-                {/*<button type="button" className="btn btn-warning signup-button">*/}
-                  <a className="nav-link" href={URL_FAVORITES}>
-                    Favorites
-                  </a>
-                {/*</button>*/}
-              </li>
-            }
-            {
-                username &&
-                <li className="nav-item active">
-                  {/*<button onClick={onLogoutClick} type="button" className="btn btn-warning signup-button">*/}
-                  <a className="nav-link" onClick={onLogoutClick}>
-                    Logout
-                  </a>
-                  {/*</button>*/}
-                </li>
-            }
-
-          </ul>
-
-          {
-              username &&
-              <li className="nav-item active title">
-                {/*<button type="button" className="btn btn-warning signup-button">*/}
-                <a className="nav-link username" href={URL_MY_JOBS}>
-                  {username}
+                <a className="nav-link sr-only" href="/">
+                  <span><HouseDoor/>  Home</span>
                 </a>
-                {/*</button>*/}
               </li>
-          }
+              {
+                  !username && (
+                      <li className="nav-item active" onClick={onLoginClick}>
+                        <a className="nav-link">
+                          {/*<span><FileEarmarkLock/>  Login</span>*/}
+                          <FileEarmarkLock/>  Login
+                        </a>
+                        {/*</button>*/}
+                      </li>
+                  )
+              }
+              {
+                  !username && (
+                      <li className="nav-item active">
 
-        </div>
-      </nav>
-    </div>
+                        <a className="nav-link" href={URL_SIGNUP}>
+                          {/*<span><Key/>  Sign Up</span>*/}
+                          <Key/>  Sign Up
+                        </a>
+                        {/*</button>*/}
+                      </li>
+                  )
+              }
+              {
+                  username &&
+                  <li className="nav-item active">
+                    {/*<button type="button" className="btn btn-warning signup-button">*/}
+                    <a className="nav-link" href={URL_FAVORITES}>
+                      <span><Heart/>  Favorites</span>
+                    </a>
+                    {/*</button>*/}
+                  </li>
+              }
+              {
+                  username &&
+                  <li className="nav-item active">
+                    {/*<button onClick={onLogoutClick} type="button" className="btn btn-warning signup-button">*/}
+                    <a className="nav-link" onClick={onLogoutClick}>
+                      {/*<span><DoorClosed/>  Logout</span>*/}
+                      <DoorClosed/>  Logout
+                    </a>
+                    {/*</button>*/}
+                  </li>
+              }
+
+              {
+                  username &&
+                  <li className="nav-item active">
+                    <a className="nav-link" id={"username"} href={URL_MY_JOBS}>
+                      {/*<span><PersonCheck/> {username}</span>*/}
+                      <PersonCheck/> {username}
+                    </a>
+
+                  </li>
+              }
+            </ul>
+
+
+          </div>
+        </nav>
+      </div>
   );
 }
